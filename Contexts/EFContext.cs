8d3ace0004f1ce282e7d5eff13b6e9a4dc3c8463
giulianolopes <giulianolopes_18@hotmail.com>
2017-05-26 20:57:17 -0300

    Add files via upload

---
+++ Contexts/EFContext.cs
@@ -0,0 +1,22 @@
+﻿using Giuliano.Paula.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Giuliano.Paula.Contexts
+{
+    public class EFContext : DbContext
+
+    {
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(new DropCreateDatabaseIfModelChanges<EFContext>());
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Supplier> Suppliers  { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
